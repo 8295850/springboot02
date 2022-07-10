2ee0d6851b8b4f3b8a28a39f2c22a316bc262a45
daiweihao <8295850@qq.com>
2022-07-10 22:49:42 +0800

    master 更新49

--- src/main/java/com/study/domain/Book.java
+++ src/main/java/com/study/domain/Book.java
@@ -49,6 +49,8 @@ public class Book {
         System.out.println("hot 进行了更新22：43");
         System.out.println("hot 进行了更新22：46");
         System.out.println("hot 进行了更新22：47");
+        System.out.println("hot 进行了更新22：48");
+
         return "domain{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
